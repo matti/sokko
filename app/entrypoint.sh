3cb6efb49bb889159d4752251cc6cb07e3f1ddc8
Matti Paksula <matti.paksula@iki.fi>
2023-04-24 13:48:22 +0300

    k8s-leader

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -34,31 +34,36 @@ case "${1:-}" in
   ;;
 esac
 
-(
-  echo "$(date) started"
+k8s_leader_lease_name=${K8S_LEADER_LEASE_NAME:-}
+
+if [[ "$k8s_leader_lease_name" != "" ]]
+then
+  (
+    echo "$(date) started"
+    while true; do
+      k8s-leader  --lease-name "${k8s_leader_lease_name}" --lease-namespace "$NAMESPACE" --identity "$HOSTNAME" || true
+      echo "$(date) exited"
+      sleep 1
+    done
+  ) >/tmp/k8s-leader.log 2>&1 &
+  echo $! >/tmp/k8s-leader.pid
+
+  echo """
+
+  TRYING TO ACQUIRE LEADERSHIP WITH k8s-leader with lease name '${k8s_leader_lease_name} in namespace '${NAMESPACE}' using identity '${HOSTNAME}' ..."
   while true; do
-    k8s-leader  --lease-name "socks-$CLUSTER" --lease-namespace "$NAMESPACE" --identity "$HOSTNAME" || true
-    echo "$(date) exited"
+    if [[ -f /tmp/k8s-leader ]]; then
+      leader_hostname=$(cat /tmp/k8s-leader)
+      if [[ "$leader_hostname" == "$HOSTNAME" ]]; then
+        break
+      fi
+    fi
+
     sleep 1
   done
-) >/tmp/k8s-leader.log 2>&1 &
-echo $! >/tmp/k8s-leader.pid
+  echo "...WE ARE THE LEADER!"
+fi
 
-
-echo """
-
-TRYING TO ACQUIRE LEADERSHIP WITH k8s-leader ..."
-while true; do
-  if [[ -f /tmp/k8s-leader ]]; then
-    leader_hostname=$(cat /tmp/k8s-leader)
-    if [[ "$leader_hostname" == "$HOSTNAME" ]]; then
-      break
-    fi
-  fi
-
-  sleep 1
-done
-echo "...WE ARE THE LEADER!"
 
 [[ "${PORT:-}" = "" ]] && export PORT=9090
 
